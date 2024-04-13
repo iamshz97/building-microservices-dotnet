bc726c4ecff24ba06e6f2ea6748d9cb2e0185fdd
Shazni Shiraz <shaznishiraz+github@gmail.com>
2024-04-14 00:08:37 +0530

    Catalog service minor changes

--- Play.Catalog.Service/Dtos.cs
+++ Play.Catalog.Service/Dtos.cs
@@ -1,11 +1,10 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace Play.Catalog.Service
-{
-    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+namespace Play.Catalog.Service;
 
-    public record CreateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
+public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
 
-    public record UpdateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
-}
+public record CreateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
+
+public record UpdateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
