99356f098ad2ff5bb3d8e97def56505d16847e7f
Shazni Shiraz <shaznishiraz+github@gmail.com>
2024-04-13 22:07:43 +0530

    Create Item.cs

---
+++ Play.Catalog.Service/Entities/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Item
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateTimeOffset CreatedDate { get; set; }
+}
